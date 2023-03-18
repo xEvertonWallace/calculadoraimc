b634566bef05c439c873ac617b10beff7f2945f1
Everton Wallace Oliveira <everton542@hotmail.com>
2023-03-18 00:53:22 +0000

    função que gera o relatorio a partir dos dados tratados

--- index.py
+++ index.py
@@ -14,11 +14,30 @@ class RelatorioIMC:
 
         self.df.dropna(subset=['Peso (kg)', 'Altura (m)'], inplace=True)
         return self.df
-        
+
     def calcular_imc(self, altura, peso): 
-        # trata casos em que peso e altura está fora dos parametros normais 
+        
         if altura > 2.0 and peso < 50.0: 
             altura, peso = peso, altura 
-        return peso / altura**2  
+        return peso / altura**2
+
+    def gerar_relatorio(self, arquivo_saida):
+        dados_tratados= self.tratamento()
+
+        with open(arquivo_saida, 'w', encoding='utf-8') as relatorio: 
+
+            cabecalho = 'Nome' + ' ' + 'IMC\n'
+            relatorio.write(cabecalho) 
+   
+            for _, row in dados_tratados.iterrows(): 
+
+                nome_completo = row['Primeiro Nome'] + ' ' + row['Sobrenomes'] 
+                
+                imc = self.calcular_imc(row['Altura (m)'], row['Peso (kg)']) 
+                imc_str = '{:.2f}'.format(imc).replace('.', ',') 
+
+                linha = f'{nome_completo} {imc_str}\n' 
+                relatorio.write(linha)     
+
 
     

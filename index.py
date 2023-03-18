e7d3a07dee657e6ef7dc5ef1cd1317b22a3e5759
Everton Wallace Oliveira <everton542@hotmail.com>
2023-03-18 00:48:11 +0000

    função que faz o calculo do IMC

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import pandas as pd
 class RelatorioIMC: 
     def __init__(self, arquivo): 
         self.df = pd.read_csv(arquivo, sep=';')
-        
+
     def tratamento(self):
 
         self.df['Peso (kg)'] = self.df['Peso (kg)'].str.replace(',', '.').astype(float) 
@@ -13,6 +13,12 @@ class RelatorioIMC:
         self.df['Sobrenomes'] = self.df['Sobrenomes'].str.strip().str.replace(r'\s+', ' ', regex=True).str.upper() 
 
         self.df.dropna(subset=['Peso (kg)', 'Altura (m)'], inplace=True)
-        return self.df  
+        return self.df
+        
+    def calcular_imc(self, altura, peso): 
+        # trata casos em que peso e altura está fora dos parametros normais 
+        if altura > 2.0 and peso < 50.0: 
+            altura, peso = peso, altura 
+        return peso / altura**2  
 
     
